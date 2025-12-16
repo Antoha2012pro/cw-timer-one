a6d9fd065a1e3b9c1fd06d171f9df2101eb18a1c
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2025-12-16 19:20:41 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,22 @@
+let khyn = "khyn";
+console.log(khyn);
+
+const timeEl = document.querySelector('#time');
+const startBtnEl = document.querySelector('#start-btn');
+const lapBtnEl = document.querySelector('#lap-btn');
+const stopBtnEl = document.querySelector('#stop-btn');
+const lapItemsEl = document.querySelector('#laps');
+
+let initialTime = 0;
+let lapTime = 0;
+let timerId = null;
+let isActiveTimer = false;
+let countOfLaps = 0;
+
+function formattingTime(ms) {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+    const othersSeconds = Math.floor((ms % 1000) / 10);
+
+    return (`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}.${String(othersSeconds).padStart(2, "0")}`);
+};
